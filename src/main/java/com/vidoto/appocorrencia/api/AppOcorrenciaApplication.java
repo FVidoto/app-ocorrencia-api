beb7d3628fceadd66b613d7bc520830074c1a1d0
fvidoto <fvidoto@gmail.com>
2019-07-09 15:38:44 -0300

    .

---
+++ src/main/java/com/vidoto/appocorrencia/api/AppOcorrenciaApplication.java
@@ -0,0 +1,13 @@
+package com.vidoto.appocorrencia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOcorrenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOcorrenciaApplication.class, args);
+	}
+
+}
