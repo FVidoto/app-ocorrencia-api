f34598405dd1f903b12e737513f516f16890be35
fvidoto <fvidoto@gmail.com>
2019-07-18 20:19:31 -0300

    commit

--- src/main/java/com/vidoto/appocorrencia/api/AppOcorrenciaApplication.java
+++ src/main/java/com/vidoto/appocorrencia/api/AppOcorrenciaApplication.java
@@ -2,12 +2,16 @@ package com.vidoto.appocorrencia.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class AppOcorrenciaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppOcorrenciaApplication.class, args);
+		System.out.println("version: " + SpringVersion.getVersion());
+
+		
 	}
 
 }
